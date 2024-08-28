a561db2041e4a1b11df83e0e6e6141d01fb18400
galahseno <galahseno.dev@gmail.com>
2024-08-28 16:00:10 +0700

    add env variable

--- build-logic/convention/src/main/java/id/dev/convention/BuildType.kt
+++ build-logic/convention/src/main/java/id/dev/convention/BuildType.kt
@@ -17,7 +17,7 @@ internal fun Project.configureBuildTypes(
         buildFeatures {
             buildConfig = true
         }
-      //  val baseUrl = gradleLocalProperties(rootDir, providers).getProperty("BASE_URL")
+        val baseUrl = gradleLocalProperties(rootDir, providers).getProperty("BASE_URL")
         val baseUrl = System.getenv("BASE_URL")
 
         when (extensionType) {
